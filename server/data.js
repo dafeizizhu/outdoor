52884c09e68364566f75999052456f973b7179e4
maizhiying <zhiying.mai@gmail.com>
2015-07-27 20:33:40 +0800

    完成jade重构

--- server/data.js
+++ server/data.js
@@ -11,7 +11,11 @@ module.exports = {
         style: 'chuanzang',
         text: '川藏滇专线'
       },
-      contents: ['丽江', '大理', '成都', '拉萨', '昆明', '九寨沟']
+      contents: ['丽江', '大理', '成都', '拉萨', '昆明', '九寨沟'],
+      details: [{
+        title: '川藏滇专线',
+        items: ['香港 | 香港 | 香港', '阳朔 | 阳朔 | 阳朔']
+      }]
     }, {
       title: {
         style: 'guonei',
@@ -55,6 +59,270 @@ module.exports = {
     }, {
       href: '',
       text: '户外保险',
+    }],
+    galleryUrls: [
+      '/images/placeholder/gallery.png',
+      '/images/placeholder/gallery.png',
+      '/images/placeholder/gallery.png'
+    ],
+    galleryTabs: [
+      '超值度假卡',
+      '春节直降万元',
+      '养生温泉'
+    ],
+    recommendRoutes: [
+      '· 【春节*海南】2月19日“走天涯海角游魅力三亚”海南深度汽车品质游',
+      '· 【云南丽江5天】“云南－丽江古 城、香格里拉、普达措、泸沽湖”经 典五日游',
+      '· 【云南丽江5天】“云南－丽江古 城、香格里拉、普达措、泸沽湖”经 典五日游',
+      '· 【云南丽江5天】“云南－丽江古 城、香格里拉、普达措、泸沽湖”经 典五日游'
+    ],
+    recommendContents: [1, 2, 3, 4],
+    catalogs: [{
+      title: '广东周边游',
+      tabs: ['精选', '珠海', '深圳', '清远', '韶关', '惠州'],
+      adImageUrl: '/images/placeholder/catalog-contents-item-left-guangdong.png',
+      items: [{
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }]
+    }, {
+      title: '川藏滇专线',
+      tabs: ['精选', '西藏', '四川', '云南'],
+      adImageUrl: '/images/placeholder/catalog-contents-item-left-chuanzang.png',
+      items: [{
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }]
+    }, {
+      title: '国内纯玩',
+      tabs: ['精选', '广西', '海南', '港澳', '哈尔滨', '华东'],
+      adImageUrl: '/images/placeholder/catalog-contents-item-left-guonei.png',
+      items: [{
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }]
+    }, {
+      title: '出境国外游',
+      tabs: ['精选', '泰国', '沙巴', '尼泊尔', '日本', '俄罗斯'],
+      adImageUrl: '/images/placeholder/catalog-contents-item-left-guowai.png',
+      items: [{
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }]
+    }, {
+      title: '团队专区',
+      tabs: ['近期成团'],
+      adImageUrl: '/images/placeholder/catalog-contents-item-left-tuandui.png',
+      items: [{
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }]
+    }, {
+      title: '五一活动',
+      tabs: ['4/13出发', '4/13出发'],
+      adImageUrl: '/images/placeholder/catalog-contents-item-left-huodong.png',
+      items: [{
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }, {
+        title: {
+          main: '<韶关丹霞山2日游>',
+          sub: '宿山脚酒店赠一正一早'
+        },
+        price: '￥239元'
+      }]
     }]
   }
 }
