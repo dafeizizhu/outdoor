195f1fedab8366dbf3c6ecc7328dd50e66cbe39d
maizhiying <zhiying.mai@gmail.com>
2015-07-29 22:55:53 +0800

    增加活动详情页面

--- server/data/index.js
+++ server/data/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   '/index.jade': require('./home'),
-  '/activities.jade': require('./activities')
+  '/activities.jade': require('./activities'),
+  '/activity.jade': require('./activity')
 }
