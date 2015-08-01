72d8a02a86219cf9a4ddd86c57583453cdfd5576
maizhiying <zhiying.mai@gmail.com>
2015-08-01 17:14:08 +0800

    增加单个页面的轮播图效果

--- js/activity.js
+++ js/activity.js
@@ -18,3 +18,48 @@ $(function () {
 
   $details.eq(0).siblings().hide()
 })
+
+$(function () {
+  var current = 0
+  var $tabs = $('.jq-gallery-tab')
+  var $list = $('.jq-gallery-main-list')
+  var $items = $('.jq-gallery-main-list-item')
+  var $prev = $('.jq-gallery-prev')
+  var $next = $('.jq-gallery-next')
+
+  function select(index) {
+    if (index < 0 || index > $tabs.length - 1) return
+    
+    current = index
+    $tabs.eq(index).addClass('selected').siblings().removeClass('selected')
+    
+    var left = 0
+    $.each($items, function (i, item) {
+      if (i < index) {
+        left += $(item).outerWidth()
+      }
+    })
+
+    $list.animate({
+      left: -left + 'px'
+    }, 1000)
+  }
+
+  $tabs.on('click', function (evt) {
+    var target = this
+    $.each($tabs, function (i, tab) {
+      if (tab == target) {
+        select(i)
+        return false
+      }
+    })
+  })
+
+  $prev.on('click', function (evt) {
+    select(current - 1)
+  })
+
+  $next.on('click', function (evt) {
+    select(current + 1)
+  })
+})
