adfae2bb173fbf59eb72816b6bd39ad415aa29fa
maizhiying <zhiying.mai@gmail.com>
2016-01-21 17:04:44 +0800

    加一个左侧banner

---
+++ js/left-side-bar.js
@@ -0,0 +1,12 @@
+$(function () {
+  var $leftSideBar = $('.jq-left-side-bar')
+
+  if ($leftSideBar.length > 0) {
+    $(window).on('resize', window_resizeHandler)
+    window_resizeHandler()
+  }
+
+  function window_resizeHandler(evt) {
+    $leftSideBar.css('top', ($(window).height() - $leftSideBar.height()) / 2 + 'px')
+  }
+})
